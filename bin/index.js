6c00390ca9509f1d42dfbb73532b47656b459775
hubvue <cckcc.kim@gmail.com>
2022-04-18 00:47:38 +0800

    fix: 执行二进制文件失败

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process')
+const { resolve } = require('path')
 
 const executeBinary = async () => {
   const [, , ...args] = process.argv
   const options = { cwd: process.cwd(), stdio: "inherit" }
 
-  return spawn('./camer', args, options)
+  return spawn(resolve(__dirname, './camer'), args, options)
 }
 
 executeBinary().catch((e) => console.error(e))
